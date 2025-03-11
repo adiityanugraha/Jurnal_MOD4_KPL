a95f0dd07a924df4530839fcadd5c0f964a546c5
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-03-12 06:56:39 +0700

    Add files via upload
    menambahkan class ke KodeProduk

---
+++ modul4_103022300063/KodeProduk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300063
+{
+    class KodeProduk
+    {
+        private static Dictionary<string, string> kodeProduk = new Dictionary<string, string>
+        {
+            {"Laptop", "E100"},
+            {"Smartphone", "E101" },
+            {"Tablet", "E102" },
+            {"Headset", "E103" },
+            {"Keyboard", "E104" },
+            {"Mouse", "E105" },
+            {"Printer", "E106" },
+            {"Monitor", "E107" },
+            {"Smartwatch", "E108" },
+            {"Kamera", "E109" }
+        };
+
+        public static string GetKodeProduk(string namaProduk)
+        {
+            if (kodeProduk.TryGetValue(namaProduk, out string KodeProduk))
+            {
+                return KodeProduk;
+            }
+            else
+            {
+                return "Produk tidak ditemukan";
+            }
+        }
+    }
+}
